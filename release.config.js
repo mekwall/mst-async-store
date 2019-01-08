1403211bc19cfffe86ba6897418ca7ac9405877f
mekwall <marcus.ekwall@gmail.com>
2019-01-08 13:20:06 +0100

    fix(semantic-release): erroneous branch config

--- release.config.js
+++ release.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  release: {
-    branch: 'release',
-  },
+  branch: 'release',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
